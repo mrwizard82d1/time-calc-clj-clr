2ccf1364faa734f5052c6e10149ca0395b3b0de7
Larry Jones <larry.jones@reveal-energy.com>
2019-08-30 19:54:10 -0500

    ClojureCLR 1.8.0 / lein-clr 0.2.2

--- project.clj
+++ project.clj
@@ -6,13 +6,13 @@
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :warn-on-reflection true
   :min-lein-version "2.0.0"
-  :plugins [[lein-clr "0.2.1"]]
+  :plugins [[lein-clr "0.2.2"]]
   :main time-calc.core
   :aot [time-calc.core]
   :clr {:cmd-templates  {:clj-exe   [[?PATH "mono"] [CLJCLR %1]]
                          :clj-dep   [[?PATH "mono"] ["target/clr/clj/Debug 4.0" %1]]
-                         :clj-url   "http://sourceforge.net/projects/clojureclr/files/clojure-clr-1.4.1-Debug-4.0.zip/download"
-                         :clj-zip   "clojure-clr-1.4.1-Debug-4.0.zip"
+                         :clj-url   "http://sourceforge.net/projects/clojureclr/files/clojure-clr-1.8.0-Debug-4.0.zip/download"
+                         :clj-zip   "clojure-clr-1.8-Debug-4.0.zip"
                          :curl      ["curl" "--insecure" "-f" "-L" "-o" %1 %2]
                          :nuget-ver [[?PATH "mono"] [*PATH "nuget.exe"] "install" %1 "-Version" %2]
                          :nuget-any [[?PATH "mono"] [*PATH "nuget.exe"] "install" %1]
